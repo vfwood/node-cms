c70e9bf8f341b87fbe3d2d359c335c248b4d700d
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-09-02 18:38:24 +0530

    Adding ejs as a template dependency and changing the extensions for public html files to ejs

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-var http = require('http');
-var path = require('path');
-var express = require('express')
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+var http = require("http");
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// set the views path
+app.set("views", publicPath);
 
-http.createServer(app).listen(8080, function() { 
+// set the view engine as ejs
+app.set("view engine", "ejs");
+
+http.createServer(app).listen(8080, function () {
   console.log("NodeCMS app started on port 8080.");
 });
