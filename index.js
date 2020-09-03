3bd276f8ead68fd75e4afe98c2615d48bd3a1d15
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-09-03 09:09:02 +0530

    Addition of the routes for index, login and signup page

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.set("views", publicPath);
 // set the view engine as ejs
 app.set("view engine", "ejs");
 
+// Root index route
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+// Route for login page
+app.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+// Route for signup page
+app.get("/signup", (req, res, next) => {
+  res.render("signUp");
+});
+
 http.createServer(app).listen(8080, function () {
   console.log("NodeCMS app started on port 8080.");
 });
